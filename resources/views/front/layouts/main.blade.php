0d5b5e3f2280b9001a87a5775a41b7b4b8cf7cc4
ravindragiri2913 <goswamirvi@gmail.com>
2025-12-05 12:52:58 +0530

    feat: add initial frontend layout, styling, and home page

--- resources/views/front/layouts/main.blade.php
+++ resources/views/front/layouts/main.blade.php
@@ -9,60 +9,60 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	@php
-		$customeseo = getSeo(url()->current());
-		if ($customeseo['seo']) {
-			$seo['title'] = $customeseo['seo']['meta_title'];
-			$seo['description'] = $customeseo['seo']['meta_description'];
-			$seo['keywords'] = $customeseo['seo']['meta_keywords'];
-		}
+	$customeseo = getSeo(url()->current());
+	if ($customeseo['seo']) {
+	$seo['title'] = $customeseo['seo']['meta_title'];
+	$seo['description'] = $customeseo['seo']['meta_description'];
+	$seo['keywords'] = $customeseo['seo']['meta_keywords'];
+	}
 	@endphp
 
 	@if (isset($seo) && !empty($seo))
-		<title>{{ $seo['title'] }}</title>
+	<title>{{ $seo['title'] }}</title>
 
-		<meta name="description" content="{{ $seo['description'] }}" />
-		<meta name="keywords" content="{{ $seo['keywords'] }}" />
+	<meta name="description" content="{{ $seo['description'] }}" />
+	<meta name="keywords" content="{{ $seo['keywords'] }}" />
 
-		<meta name="distribution" content="global">
-		<meta http-equiv="content-language" content="en-gb">
-		<meta name="city" content="{{ $seo['city'] }}">
-		<meta name="state" content="{{ $seo['state'] }}">
-		<meta name="geo.region" content="IN-GJ">
-		<meta name="DC.title" content="{{ $seo['title'] }}">
-		<meta name="geo.position" content="{{ $seo['position'] }}">
-		<meta name="ICBM" content="{{ $seo['position'] }}">
-		<meta name="geo.region" content="IN-{{ strtoupper(substr($seo['state'], 0, 2)) }}">
-		<meta name="geo.placename" content="{{ $seo['city'] }}">
+	<meta name="distribution" content="global">
+	<meta http-equiv="content-language" content="en-gb">
+	<meta name="city" content="{{ $seo['city'] }}">
+	<meta name="state" content="{{ $seo['state'] }}">
+	<meta name="geo.region" content="IN-GJ">
+	<meta name="DC.title" content="{{ $seo['title'] }}">
+	<meta name="geo.position" content="{{ $seo['position'] }}">
+	<meta name="ICBM" content="{{ $seo['position'] }}">
+	<meta name="geo.region" content="IN-{{ strtoupper(substr($seo['state'], 0, 2)) }}">
+	<meta name="geo.placename" content="{{ $seo['city'] }}">
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta property="al:web:url" content="{{ url()->current() }}">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta property="al:web:url" content="{{ url()->current() }}">
 
-		<meta name="copyright" content="{{$customeseo['domain']}}">
+	<meta name="copyright" content="{{$customeseo['domain']}}">
 
-		<meta property="og:title" content="{{ $seo['title'] }}" />
-		<meta property="og:description" content="{{ $seo['description'] }}" />
-		<meta property="og:keywords" content="{{ $seo['keywords'] }}" />
-		<meta property="og:url" content="{{ url()->current() }}">
-		<meta property="og:type" content="Car and Taxi Booking Website" />
-		<meta property="og:site_name" content="{{$customeseo['domain']}} - Car and Taxi Booking Website">
-		<meta property="og:locale" content="en_GB">
-		<meta property="og:image" content="{{ config('const.site_setting.fevicon') }}">
-		<!-- <meta property="og:image:width" content="550" />
+	<meta property="og:title" content="{{ $seo['title'] }}" />
+	<meta property="og:description" content="{{ $seo['description'] }}" />
+	<meta property="og:keywords" content="{{ $seo['keywords'] }}" />
+	<meta property="og:url" content="{{ url()->current() }}">
+	<meta property="og:type" content="Car and Taxi Booking Website" />
+	<meta property="og:site_name" content="{{$customeseo['domain']}} - Car and Taxi Booking Website">
+	<meta property="og:locale" content="en_GB">
+	<meta property="og:image" content="{{ config('const.site_setting.fevicon') }}">
+	<!-- <meta property="og:image:width" content="550" />
 		<meta property="og:image:height" content="413" /> -->
 
-		<meta name="author" content="{{$customeseo['domain']}} Car and Taxi Booking " />
+	<meta name="author" content="{{$customeseo['domain']}} Car and Taxi Booking " />
 
-		<meta property="twitter:card" content="summary">
-		<meta property="twitter:site" content="{{$customeseo['domainwithdot']}}">
-		<meta property="twitter:title" content="{{ $seo['title'] }}">
-		<meta property="twitter:description" content="{{ $seo['description'] }}">
-		<meta property="twitter:image" content="{{ config('const.site_setting.fevicon') }}">
-		<meta property="twitter:url" content="{{ url()->current() }}">
-		<meta name="twitter:domain" content="{{$customeseo['domain']}}">
+	<meta property="twitter:card" content="summary">
+	<meta property="twitter:site" content="{{$customeseo['domainwithdot']}}">
+	<meta property="twitter:title" content="{{ $seo['title'] }}">
+	<meta property="twitter:description" content="{{ $seo['description'] }}">
+	<meta property="twitter:image" content="{{ config('const.site_setting.fevicon') }}">
+	<meta property="twitter:url" content="{{ url()->current() }}">
+	<meta name="twitter:domain" content="{{$customeseo['domain']}}">
 
-		<meta name="robots" content="index,follow" />
+	<meta name="robots" content="index,follow" />
 	@else
-		<title>{{ config('const.site_setting.name') }}</title>
+	<title>{{ config('const.site_setting.name') }}</title>
 	@endif
 
 	<link rel="apple-touch-icon" sizes="180x180" href="{{ config('const.site_setting.fevicon') }}">
@@ -91,7 +91,7 @@
 	<nav class="navbar navbar-expand-lg bg-white shadow-sm py-3 fixed-top">
 		<div class="container">
 			<a class="navbar-brand fw-bold d-flex align-items-center gap-2" href="#">
-				<span class="icon-circle bg-dark">
+				<span class="icon-circle">
 					<i class="bi bi-volume-up-fill fs-4"></i>
 				</span>
 				Quietly
@@ -112,7 +112,7 @@
 					<li class="nav-item"><a class="nav-link"
 							href="{{ Route::is('home') ? '' : route('home') }}#contact">Contact</a></li>
 					<li class="nav-item">
-						<a class="btn btn-dark px-4 ms-lg-2" href="{{ config('const.app_playstore_url') }}">Download</a>
+						<a class="btn btn-primary-custom px-4 ms-lg-2" href="{{ config('const.app_playstore_url') }}">Download</a>
 					</li>
 				</ul>
 			</div>
@@ -128,7 +128,7 @@
 				<div class="col-md-6">
 					<a class="d-inline-flex align-items-center gap-2 mb-2 text-decoration-none text-dark fw-bold"
 						href="#">
-						<span class="icon-circle bg-dark">
+						<span class="icon-circle">
 							<i class="bi bi-volume-up-fill fs-5"></i>
 						</span>
 						Quietly
